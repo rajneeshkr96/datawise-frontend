0ef795fcaee2ca18940dd5a348a216d291d10170
rajneesh <rajneeshkumar9674737@gmail.com>
2025-07-14 17:42:09 +0530

    fix:type error

--- src/features/components/UserTable.tsx
+++ src/features/components/UserTable.tsx
@@ -10,51 +10,7 @@ import {
 import { useEffect, useState } from "react"
 import { getDataset } from "../services/DashboardApi"
 import type { UserTableData } from "@/types/userTable"
-
-const invoices = [
-  {
-    invoice: "INV001",
-    paymentStatus: "Paid",
-    totalAmount: "$250.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV002",
-    paymentStatus: "Pending",
-    totalAmount: "$150.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV003",
-    paymentStatus: "Unpaid",
-    totalAmount: "$350.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV004",
-    paymentStatus: "Paid",
-    totalAmount: "$450.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV005",
-    paymentStatus: "Paid",
-    totalAmount: "$550.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV006",
-    paymentStatus: "Pending",
-    totalAmount: "$200.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV007",
-    paymentStatus: "Unpaid",
-    totalAmount: "$300.00",
-    paymentMethod: "Credit Card",
-  },
-]
+import { toast } from "sonner"
 
 
 
@@ -66,8 +22,13 @@ const UserTable = () => {
     setData(val.data);
     
   }).catch((error)=>{
-    
-    
+    toast("error", {
+      description: (
+        <pre className="mt-2 w-[320px] rounded-md bg-red-500 p-4">
+          <code className="text-white">{JSON.stringify(error, null, 2)}</code>
+        </pre>
+      ),
+    })
   })
 },[])
 
